e2684d7e7675d3966817aff27906f45909d8614f
Jorge Teixeira <jorgetbruno@hotmail.co.uk>
2017-05-31 05:02:56 +0000

    Updated routes, changed comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :comments
   root 'posts#index'
+  resources :comments, only: [:create, :destroy, :update]
   resources :posts
   devise_for :users
 end
