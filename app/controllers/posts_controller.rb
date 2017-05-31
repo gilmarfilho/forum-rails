f4ab99412534240180f6c5dab702d5a1f2755e68
Jorge Teixeira <jorgetbruno@hotmail.co.uk>
2017-05-31 04:47:38 +0000

    Updated controller posts, added comments in show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,21 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.all.page(params[:page]).per(10)
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
+    @comments = Comment.where(post_id: @post.id).order(:created_at)
+    @comment = Comment.new
+    @comment.post = @post
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
